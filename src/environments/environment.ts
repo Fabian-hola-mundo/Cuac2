64e014138ca1060079a83f53ede42c6280718e1e
Fabián Miranda <fabo.0220@gmail.com>
2026-05-22 03:27:15 -0500

    feat: install supabase-js and add environment config

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  supabaseUrl: 'https://ytqcwrjxlnlsjgnjxiiw.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0cWN3cmp4bG5sc2pnbmp4aWl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MTE1MzMsImV4cCI6MjA5NDk4NzUzM30.hW01ztylAFWhy3bAjJXPl4Q8wh2YP_DbpR2oaXWHxVs',
+};
